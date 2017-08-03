4f1f80f92a77182d9982944dd54a2d83f900ec6b
0ff3nd3r <86129qx7a4s@gmail.com>
2017-08-03 17:49:54 -0400

    Added a card with two photos, bug-fix
    Added view for a card with two photos. Fixed a bug concerning "display: flex" property that  resulted in the collapse of the grid with photos. Added bootstrap

--- VK/www/js/my-app.js
+++ VK/www/js/my-app.js
@@ -13,12 +13,15 @@ var mainView = myApp.addView('.view-main', {
 });
 
 // Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
-});
+// myApp.onPageInit('about', function (page) {
+//     // run createContentPage func after link was clicked
+//     $$('.create-page').on('click', function () {
+//         createContentPage();
+//     });
+// });
+
+centerElements();
+// fitWidth();
 
 // Generate dynamic page
 var dynamicPageIndex = 0;
@@ -48,3 +51,57 @@ function createContentPage() {
     );
 	return;
 }
+
+function centerElements() {
+  var elements = $('.v-centered');
+  for (var i = 0; i < elements.length; i++) {
+    var element = $(elements[i]);
+    var parentHeight = element.parent().height();
+    element.css('margin', '' + (parentHeight - element.outerHeight()) / 2 + 'px 0');
+  }
+  return;
+}
+
+// Finds a row with two photos and resizes the photos to fit the width of the card 
+function fitWidth() {
+  // Find rows with 1-2 photos
+  var rows = $('.one-row .row-one');
+  
+  for (var i = 0; i < rows.length; i++) {
+    // Get parent and children
+    var parentBlock = $(rows[i]);
+    var parentWidth = parentBlock.width();
+    var children = parentBlock.children();
+    
+    // Total width of children
+    var childrenWidth = 0;
+    for (var i = 0; i < children.length; i++) {
+      childrenWidth += $(children[i]).width();
+    }
+    
+    // Find the difference in width. Devide for number of children to find
+    // needed additional length for each child block
+    var additionalLength = (parentWidth - childrenWidth) / children.length - 1;
+    
+    // Change the width of children
+    for (var i = 0; i < children.length; i++) {
+      $(children[i]).width(
+        $(children[i]).width() + additionalLength
+      );
+    }
+  }
+}
+
+// // Functions to correct the size of the cards and photos
+// function resizeCards() {
+//   var HEIGHT = "438px";
+//   var cards = $$('.news-card-item-photo');
+  
+//   // Iterate over cards
+//   // Temp solution. Height property should be in css
+//   for (i = 0; i < cards.length; i++) {
+//     cards[i].style.height = HEIGHT;
+//   }
+  
+  
+// }
